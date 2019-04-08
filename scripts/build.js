5a9890f85bfa22055194d883c6ffdb7fcf9bb8a6
Sarah Dayan <dayan.sarah@gmail.com>
2019-04-09 00:00:37 +0200

    Fix build

--- scripts/build.js
+++ scripts/build.js
@@ -31,30 +31,42 @@ const modules = [
     path: 'src/browserstore.js',
     plugins: [...pluginSetups.default],
     dest: 'browserstore',
+    name: 'browserstore',
     builds: ['cjs', 'umd', 'amd', 'es']
   },
   {
     path: 'src/createStore.js',
     plugins: [...pluginSetups.default],
     dest: 'lite/createStore',
+    name: 'createStore',
     builds: ['umd', 'amd']
   },
   {
     path: 'src/multiStore.js',
     plugins: [...pluginSetups.default],
     dest: 'lite/multiStore',
+    name: 'multiStore',
     builds: ['umd', 'amd']
   },
   {
     path: 'src/adapters/localStorage.js',
     plugins: [...pluginSetups.default],
     dest: 'adapters/localStorage',
+    name: 'localStorageAdapter',
+    builds: ['cjs', 'umd', 'amd', 'es']
+  },
+  {
+    path: 'src/adapters/url.js',
+    plugins: [...pluginSetups.default],
+    dest: 'adapters/url',
+    name: 'urlAdapter',
     builds: ['cjs', 'umd', 'amd', 'es']
   },
   {
     path: 'src/adapters/sessionStorage.js',
     plugins: [...pluginSetups.default],
     dest: 'adapters/sessionStorage',
+    name: 'sessionStorageAdapter',
     builds: ['cjs', 'umd', 'amd', 'es']
   }
 ]
@@ -89,25 +101,25 @@ const outputs = [
   }
 ]
 
-modules.forEach(({ path, builds, plugins: componentPlugins, dest }) => {
+modules.forEach(({ path, builds, plugins: componentPlugins, dest, name }) => {
   processes.forEach(({ plugins: processPlugins, suffix }) => {
     ;(async () => {
       const bundle = await rollup.rollup({
         input: path,
         plugins: [...componentPlugins, ...processPlugins]
       })
-      buildOutputs(bundle, builds, dest + suffix)
+      buildOutputs({ bundle, builds, path: dest + suffix, name })
     })()
   })
 })
 
-const buildOutputs = (bundle, builds, path = '') => {
+const buildOutputs = ({ bundle, builds, path = '', name } = {}) => {
   outputs.forEach(({ folder, format }) => {
     if (builds.includes(format)) {
       bundle.write({
         file: `build/${folder}/${path}.js`,
         format,
-        name: 'browserstore'
+        name
       })
     }
   })
